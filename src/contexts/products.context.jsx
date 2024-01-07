0807bfee8026a880b17bd1171c94c9e1ab2e4fb9
Ivan Tang <ivantanghw@gmail.com>
2024-01-07 12:20:39 -0500

    redundant changes

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+import PRODUCTS from '../shop-data';
 
 export const ProductsContext = createContext({
   products: [],
