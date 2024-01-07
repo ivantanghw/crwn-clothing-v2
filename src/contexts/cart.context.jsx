1aba7fb4e0179d021f81359aa27855320a1b92b2
Ivan Tang <ivantanghw@gmail.com>
2024-01-07 15:36:32 -0500

    added addToCart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 /* */
 const addCartItem = (cartItems, productToAdd) => {
     // find if cartItems contains productToAdd 
@@ -26,11 +26,19 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity, 0);
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
@@ -40,6 +48,7 @@ export const CartProvider = ({ children }) => {
     isCartOpen, 
     setIsCartOpen, 
     cartItems, 
-    addItemToCart };
+    addItemToCart,
+    cartCount };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
