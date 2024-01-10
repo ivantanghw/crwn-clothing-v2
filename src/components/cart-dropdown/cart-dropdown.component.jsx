31f55d63d5c78ab6af632e86beef1f5224962371
Ivan Tang <ivantanghw@gmail.com>
2024-01-09 22:09:40 -0500

    finish checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -8,6 +9,11 @@ import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckOutHandler = () => {
+        navigate('/checkout')
+    };
 
     return (
         <div className='cart-dropdown-container'>
@@ -20,7 +26,7 @@ const CartDropdown = () => {
                     <span className='empty-message'>Your cart is empty</span>
                     )}
             </div>
-            <Button>Go to checkout</Button>
+            <Button onClick={goToCheckOutHandler}>Go to checkout</Button>
         </div>
     )
 };
